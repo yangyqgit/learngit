673dc3c6ae9d8506cbf07374cfe2678208c4232a
yyq <yyq@163.com>
2015-07-24 16:30:51 +0800

    socket

--- client.py
+++ client.py
@@ -1,18 +1,14 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-'a socket example which send echo message to server.'
-
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 建立连接:
-s.connect(('127.0.0.1', 9999))
-# 接收欢迎消息:
-print s.recv(1024)
-for data in ['Michael', 'Tracy', 'Sarah']:
-    # 发送数据:
-    s.send(data)
-    print s.recv(1024)
-s.send('exit')
-s.close()
+def start_client():
+	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	s.connect(('127.0.0.1',9998))
+	print(s.recv(1024).decode('utf-8'))
+	for data in [b'Michael',b'Tracy',b'Sarah']:
+		s.send(data)
+		print(s.recv(1024).decode('utf-8'))
+	s.send(b'exit')
+	s.close()
+
+if __name__=='__main__':
+	start_client()
--- server.py
+++ server.py
@@ -1,31 +1,27 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import socket
+import threading,time
 
-'a server example which send hello to client.'
+def tcplink(sock,addr):
+	print('Accept new connection from %s:%s'% addr)
+	sock.send(b'Welcome!')
+	while True:
+		data = sock.recv(1024)
+		time.sleep(1)
+		if not data or data.decode('utf-8') == 'exit':
+			break
+		sock.send(('hello, %s' % data).encode('utf-8'))
+	sock.close()
+	print('connection from %s:%s closed.'%addr)
 
-import time, socket, threading
+def start():
+	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	s.bind(('127.0.0.1',9998))
+	s.listen(5)
+	print('waiting for connection...')
+	while True:
+		sock,addr = s.accept()
+		t = threading.Thread(target=tcplink,args=(sock,addr))
+		t.start()
 
-def tcplink(sock, addr):
-    print 'Accept new connection from %s:%s...' % addr
-    sock.send('Welcome!')
-    while True:
-        data = sock.recv(1024)
-        time.sleep(1)
-        if data == 'exit' or not data:
-            break
-        sock.send('Hello, %s!' % data)
-    sock.close()
-    print 'Connection from %s:%s closed.' % addr
-    print 'Waiting for connection...'
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 监听端口:
-s.bind(('127.0.0.1', 9999))
-s.listen(5)
-print 'Waiting for connection...'
-while True:
-    # 接受一个新连接:
-    sock, addr = s.accept()
-    # 创建新线程来处理TCP连接:
-    t = threading.Thread(target=tcplink, args=(sock, addr))
-    t.start()
+if __name__=='__main__':
+	start()
